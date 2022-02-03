78189ffc95398b9eb2abc485ec162172090ff02c
Robert Belanec <belanecrobert22@gmail.com>
2022-02-03 12:42:10 +0100

    fix

--- getcve.sh
+++ getcve.sh
@@ -51,4 +51,6 @@ relevant_chagelog="${full_changelog%${current_version}*}"
 # echo "${full_changelog}"
 echo "${relevant_chagelog}"
 
+cves=`grep '\- CVE' "${relevant_chagelog}"" | cut -d' ' -f6`
+echo "${cves}"
 
