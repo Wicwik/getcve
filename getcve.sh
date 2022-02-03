98ac1b442a01e2912128d7ba245a3aeb43fcff16
Robert Belanec <belanecrobert22@gmail.com>
2022-02-03 11:31:22 +0100

    added file and some functions

---
+++ getcve.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+
+POSITIONAL_ARGS=()
+
+while [[ $# -gt 0 ]]; do
+	case $1 in
+		-*|--*)
+			echo "Unknown option $1"
+	     	exit 1
+	    	;;
+
+	    *)
+	    	POSITIONAL_ARGS+=("$1")
+	    	shift
+	        ;;
+    esac
+done
+
+package="${POSITIONAL_ARGS[0]}"
+current_version=""
+candidate_version=""
+
+
+function get_current_version
+{
+	current_version=${apt-cache policy "${package}" | grep "Installed"}
+}
+
+function get_candidate_version
+{
+	candidate_version=${apt-cache policy "${package}" | grep "Candidate"}
+
+}
+
+echo current_version
+echo candidate_version
+
